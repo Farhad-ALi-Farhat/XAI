e500c226284e2372d17f7156f72b68079cfd6443
Farhad Ali Farhat <farhadalifarhat@gmail.com>
2025-07-13 20:25:06 +0500

    Update model.py

--- app/model.py
+++ app/model.py
@@ -2,8 +2,12 @@ from tensorflow.keras.models import load_model
 import os
 import gdown
 
-model_url = "https://drive.google.com/file/d/1UFetDurNduZXbEwWxV8zxRxdyL8WpreJ/view?usp=sharing"  # Replace with real file ID
+model_url = "https://drive.google.com/uc?id=1UFetDurNduZXbEwWxV8zxRxdyL8WpreJ"  # direct link
 model_path = "best_model.keras"
 
-if not os.path.exists(model_path):
-    gdown.download(model_url, model_path, quiet=False)
+def get_model():
+    if not os.path.exists(model_path):
+        print("Downloading model...")
+        gdown.download(model_url, model_path, quiet=False)
+    print("Loading model...")
+    return load_model(model_path)
